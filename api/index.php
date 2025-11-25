c7de25d9fcbe1cb4ca37e6cfa7bb690f83d12715
fl4wless <noah.bauditz@live.de>
2025-11-25 10:25:05 +0100

    vercel: adjusts deployment

--- api/index.php
+++ api/index.php
@@ -3,8 +3,12 @@
 // Vercel Serverless Function Entry Point
 // This file is the entry point for all HTTP requests on Vercel
 
-// Include Composer autoloader from the root
-require __DIR__ . '/../vendor/autoload.php';
+// Include Composer autoloader - check api/vendor first, then root vendor
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require __DIR__ . '/vendor/autoload.php';
+} else {
+    require __DIR__ . '/../vendor/autoload.php';
+}
 
 // Bootstrap Laravel application
 $app = require_once __DIR__ . '/../bootstrap/app.php';
